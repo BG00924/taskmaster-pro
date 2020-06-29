b262cd2b54f2d87a263668a0d3f6cd7bb59da948
BG00924 <BG00924@users.noreply.github.com>
2020-06-29 14:42:28 -0500

    added edit function to task and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    //console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,7 +45,102 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// event listener for editting
+$(".list-group").on("click", "p", function () {
+  // uses .text() to get inner text of current element represented by $(this)
+  var text = $(this)
+    .text()
+    .trim();
+  // creates the <textarea> and adds a class and gives it a value of var text
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  // targets this object and replace <p> with <textarea>
+  $(this).replaceWith(textInput);
+  // highlights the input box created in var textInput
+  textInput.trigger("focus");
+  //console.log(this);
+});
 
+// event listener that we will use in leiu of save button
+//blur event allows it to trigger as soon as interacts with anything other than <textarea>
+$(".list-group").on("blur", "textarea", function() {
+// get the textarea's current value/text
+var text = $(this)
+  .val()
+  .trim();
+// get the parent ul's id attribute 
+// 5.1.6. i believe this selects the list-group class of the ul and pulls 
+// the id associated with it removes the list- to select the toDo array
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+// tasks is an object; tasks[status] returns an array (e.g., toDo);
+// tasks[status][index] returns the object at the given index in the array;
+// tasks[status][index].text returns the text property of the object at the given index
+// this is necessary so when we saveTasks it goes to localStorage
+tasks[status][index].text = text;
+saveTasks();
+
+// recreate p element
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+// replace textarea with p element
+$(this).replaceWith(taskP);
+});
+
+// event listener to edit due date
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+  //create new input element
+  // In jQuery, attr() can serve two purposes. With one argument, 
+  //  it gets an attribute (e.g., attr("id")). With two arguments, 
+  //  it sets an attribute (e.g., attr("type", "text")). 
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  // swap out elements
+  $(this).replaceWith(dateInput);
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function () {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 
 // modal was triggered
